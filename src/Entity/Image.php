691ef7b1fef3fc901aede40a23527df26db48151
Steve Deliwrapp <steve.kaci@deliwrapp.com>
2022-11-23 11:19:54 +0100

    constraints added to image

--- src/Entity/Image.php
+++ src/Entity/Image.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\ImageRepository;
 use DateTime;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: ImageRepository::class)]
 class Image
@@ -15,9 +16,23 @@ class Image
     private ?int $id = null;
 
     #[ORM\Column(length: 50)]
-    private ?string $title = null;
+    #[Assert\NotBlank()]
+    #[Assert\Length(
+        min: 3,
+        max: 50,
+        minMessage: 'Your first name must be at least {{ limit }} characters long',
+        maxMessage: 'Your first name cannot be longer than {{ limit }} characters',
+    )]
 
+    private ?string $title = null;
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank()]
+    #[Assert\Length(
+        min: 5,
+        max: 255,
+        minMessage: 'Your first name must be at least {{ limit }} characters long',
+        maxMessage: 'Your first name cannot be longer than {{ limit }} characters',
+    )]
     private ?string $description = null;
 
     #[ORM\Column]
