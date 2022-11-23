cbe510a3693aa1d49a879a8a40c6cb1cc68c06a0
Steve Deliwrapp <steve.kaci@deliwrapp.com>
2022-11-23 12:37:26 +0100

    some css changes

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,12 +5,15 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ImageRepository;
 
 class HomeController extends AbstractController
 {
     #[route('/', 'home', methods: ['GET'])]
-    public function index(): Response
+    public function index(ImageRepository $imageRepository): Response
     {
-        return $this->render("home.html.twig");
+        $images = $imageRepository->findAll();
+
+        return $this->render("home.html.twig",["images"=>$images]);
     }
 }
