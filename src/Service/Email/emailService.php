10ec3498de3a29d5419612582659a73f61c903d3
Steve Deliwrapp <steve.kaci@deliwrapp.com>
2022-12-19 09:19:17 +0100

    email service modified

--- src/Service/Email/emailService.php
+++ src/Service/Email/emailService.php
@@ -22,14 +22,16 @@ class emailService
      * @param Contact $contact
      * @return void
      */
-    public function sendContact(Contact $contact, string $emailContact): void
+    public function sendContact(Contact $contact,string $emailContact): void
     {
-            $this->mailer->send($this->createEmail(
-                $contact->getEmail(),
-                $emailContact,
-                $contact->getSubject() ? $contact->getSubject() : 'no subject',
-                $contact->getMessage(),
-                '<p>' . $contact->getMessage() . '</p>'));      
+        $email = (new Email())
+            ->from($contact->getEmail())
+            ->to($emailContact)
+            ->subject($contact->getSubject()?$contact->getSubject():'no subject')
+            ->text($contact->getMessage())
+            ->html('<p>'.$contact->getMessage().'</p>');
+
+        $this->mailer->send($email);
     }
 
     /**
@@ -40,27 +42,19 @@ class emailService
      */
     public function sendRegister(User $user)
     {
-        $this->mailer->send($this->createEmail(
-            'instapic@aol.fr',
-            $user->getEmail(),
-            'Welcome to instaPic !',
-            'Hello ' . $user->getpseudo() . ' Thank you for registering to instaPic !',
-            '<h1>Hello ' . $user->getpseudo() . '</h1> <p>Thank you for registering to instaPic !</p>'));
+        $email = (new Email())
+        ->from('instapic@aol.fr')
+        ->to($user->getEmail())
+        ->subject('Welcome to instaPic !')
+        ->text('Hello '.$user->getpseudo().' Thank you for registering to instaPic !')
+        ->html('<h1>Hello '.$user->getpseudo().'</h1> <p>Thank you for registering to instaPic !</p>');
+        
+        $this->mailer->send($email);
     }
 
     public function sendPassword(User $user)
     {
         //Todo
-    }
-
-    private function createEmail(string $from, string $to, string $subject, string $messageTxt, string $messageHtml): Email
-    {
-        return (new Email())
-            ->from($from)
-            ->to($to)
-            ->subject($subject)
-            ->text($messageTxt)
-            ->html($messageHtml);
     }
 
 }
