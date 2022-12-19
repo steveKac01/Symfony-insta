1b6ad2e459e202a799073faaf1724f5c77c75d02
Steve Deliwrapp <steve.kaci@deliwrapp.com>
2022-12-19 08:59:31 +0100

    recaptcha v3 added in contact form.

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Contact;
 
 use Symfony\Component\Form\AbstractType;
+use Karser\Recaptcha3Bundle\Form\Recaptcha3Type;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints as Assert;
@@ -12,6 +13,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Karser\Recaptcha3Bundle\Validator\Constraints\Recaptcha3;
 
 
 class ContactType extends AbstractType
@@ -98,6 +100,12 @@ class ContactType extends AbstractType
                 'attr' => [
                     'class' => 'btn btn-primary my-4'
                 ],  'label' => 'Envoyer votre message'
+            ])
+
+            ->add('captcha', Recaptcha3Type::class, [
+                'constraints' => new Recaptcha3(),
+                'action_name' => 'contact',
+                'locale' => 'fr',
             ]);
     }
 
