24ccaa83936d43b9bc4d5fff907cd7f622790650
Steve Deliwrapp <steve.kaci@deliwrapp.com>
2022-12-21 10:39:10 +0100

    command done

--- src/Command/AddAdminCommand.php
+++ src/Command/AddAdminCommand.php
@@ -4,14 +4,14 @@ namespace App\Command;
 
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\Question;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 #[AsCommand(
     name: 'app:add-admin',
@@ -20,11 +20,13 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 class AddAdminCommand extends Command
 {
     private EntityManagerInterface $manager;
+    private ValidatorInterface $validator;
 
-    public function __construct(EntityManagerInterface $manager)
+    public function __construct(EntityManagerInterface $manager, ValidatorInterface $validator)
     {
-       parent::__construct();
+        parent::__construct();
         $this->manager = $manager;
+        $this->validator = $validator;
     }
 
     protected function configure(): void
@@ -43,20 +45,20 @@ class AddAdminCommand extends Command
         $email = $input->getArgument('email');
         $password = $input->getArgument('password');
 
-        if(!$pseudo)
-        {
+        if (!$pseudo) {
+            $io->title('[ Adding an administrator ]');
             $question = new Question('Enter the pseudo of the new administrator :');
             $pseudo = $helper->ask($input, $output, $question);
         }
 
-        if(!$email)
-        {
-            $question = new Question('Enter the email of the new user :');
+        if (!$email) {
+
+            $question = new Question('Enter the email for ' . $pseudo . ' :');
             $email = $helper->ask($input, $output, $question);
         }
-        if(!$password)
-        {
-            $question = new Question('Enter the password of the new user :');
+        if (!$password) {
+            $io->note('Choose a secure password !');
+            $question = new Question('Enter the password for ' . $pseudo . ' :');
             $password = $helper->ask($input, $output, $question);
         }
 
@@ -67,8 +69,17 @@ class AddAdminCommand extends Command
                 ->setPlainPassword($password)
                 ->setRoles(['ROLE_ADMIN']);
 
-            $this->manager->persist($user);
-            $this->manager->flush();
+            //Validate the user data.
+            $errors = $this->validator->validate($user);
+            if (count($errors)>0) {
+                $io->error((string) $errors);
+
+                return Command::FAILURE;
+
+            } else {
+                $this->manager->persist($user);
+                $this->manager->flush();
+            }
         }
 
         $io->success('New admin successfully created ! ' . $user->getEmail());
