b61d916fd6a13c22ee8f7635b1bc40406aebcf9d
Steve Deliwrapp <steve.kaci@deliwrapp.com>
2022-12-22 10:32:01 +0100

    filter & files added

---
+++ src/Interfaces/CacheConfig.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Interfaces;
+
+/**
+ * Config files for the cache.
+ */
+interface CacheConfig
+{
+    public const CACHE_POSTS_KEY = "posts_home";
+}
---
+++ src/Interfaces/FixturesConfig.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Interfaces;
+
+/**
+ * Config files for datafixtures.
+ */
+interface FixturesConfig
+{
+    public const NUMBER_USER = 10;
+    public const NUMBER_POST = 20;
+    public const NUMBER_COMMENTS = 70;
+    public const CATEGORIES_LABEL = ["SCREENSHOT", "ARTWORK", "WORK IN PROGRESS", "PHOTO"];
+    public const CATEGORIES_COLOR = ["danger", "info", "success", "warning"];
+}
