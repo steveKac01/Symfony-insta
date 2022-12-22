ca5dfcc0f364d5524b174467cd846638c78ce0a4
Steve Deliwrapp <steve.kaci@deliwrapp.com>
2022-12-22 16:48:52 +0100

    fix

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -8,7 +8,6 @@ use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class CommentType extends AbstractType
 {
